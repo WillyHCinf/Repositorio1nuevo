da202b3f2b5f517e58b6bd9e9496d1b1caa41903
WillyHCinf <162583077+WillyHCinf@users.noreply.github.com>
2024-03-07 08:50:03 -0400

    Add files via upload

---
+++ lab WEB/funcion.js
@@ -0,0 +1,9 @@
+let nombres=['Omar','Raul','Samuel','Daniel', 'Roman'];
+let indice = 0;
+function sortear(){
+    seleccion = nombres[parseInt(Math.random()*nombres.length)]:
+    document.getElementById("resultado").value=seleccion;
+}
+function mostrarnombres{
+    document.getElementById("nombres").innerHTML=nombres.toString();
+}
